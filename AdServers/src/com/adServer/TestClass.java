a9bbf3f51b63c95cc708a9b9d8471077dbb5df4d
Gopi Krishna Davuluru <gkrishnadavuluru2@gmail.com>
2018-09-19 20:44:30 -0400

    New Changes in 1.1_Release to Test Git Commands

--- AdServers/src/com/adServer/TestClass.java
+++ AdServers/src/com/adServer/TestClass.java
@@ -2,9 +2,15 @@ package com.adServer;
 
 public class TestClass {
 
+	public String nameString() {
+		String s =null;
+		return s;
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Best way to test Git commands ");
 
+		TestClass tc = new TestClass();
+		tc.nameString();
 	}
 
 }
--- AdServers/src/com/anudeeptest/NewAnudeepTest.java
+++ AdServers/src/com/anudeeptest/NewAnudeepTest.java
@@ -2,9 +2,12 @@ package com.anudeeptest;
 
 public class NewAnudeepTest {
 
+	public void newVoid() {
+		System.out.println("New Testing Git Commands Files");
+	}
 	public static void main(String[] args) {
 		
-
+		System.out.println("Public void Sysoout");
 	}
 
 }
